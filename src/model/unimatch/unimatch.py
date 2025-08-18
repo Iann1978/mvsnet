63300edd90cab6afacaefe1d0084da5fd38e9816
iann <iann1978@163.com>
2025-08-18 10:37:28 +0800

    - A little modify.

--- src/model/unimatch/unimatch.py
+++ src/model/unimatch/unimatch.py
@@ -13,18 +13,24 @@ class UniMatch(BaseModel):
         self.backbone = CNNEncoder(output_dim=128,
                  norm_layer=nn.InstanceNorm2d,
                  num_output_scales=1,
-                 return_all_scales=True,
+                 return_all_scales=False,
                  )
         self.conv1 = nn.Conv2d(128, 1, kernel_size=3, stride=1, padding=1)
+        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
         print(self.cfg.configu) 
 
     def forward(self, x: BatchedViews):
-        print(x['images'].shape)
+        # print(x['images'].shape)
         x = self.backbone(x['images'][:,0])
-        print('\n\n\n')
-        print(len(x))
-        for i in range(len(x)):
-            print(x[i].shape)
-        exit()
-        return self.conv1(x[0]).unsqueeze(1)
+        # print('\n\n\n')
+        # print(len(x))
+        # for i in range(len(x)):
+        #     print(x[i].shape)
+        x = self.conv1(x[0])
+        x = self.upsample(x)
+        x = self.upsample(x)
+        x = self.upsample(x)
+        # print(x.shape)
+        # exit()
+        return x.unsqueeze(1)
 
