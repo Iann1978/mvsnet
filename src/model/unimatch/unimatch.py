26a58a47dfafd8f96a79cf2767fdd6993878f022
iann <iann1978@163.com>
2025-08-18 09:15:13 +0800

    + Add unimath's backbone.py into project.

--- src/model/unimatch/unimatch.py
+++ src/model/unimatch/unimatch.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from ..base_model import BaseModelConfig, BaseModel, BatchedViews
 import torch.nn as nn
-
+from .backbone import CNNEncoder
 
 @dataclass
 class UniMatchConfig(BaseModelConfig):
@@ -10,9 +10,21 @@ class UniMatchConfig(BaseModelConfig):
 class UniMatch(BaseModel):
     def __init__(self, cfg: UniMatchConfig):
         super().__init__(cfg)
-        self.conv1 = nn.Conv2d(3, 1, kernel_size=3, stride=1, padding=1)
+        self.backbone = CNNEncoder(output_dim=128,
+                 norm_layer=nn.InstanceNorm2d,
+                 num_output_scales=1,
+                 return_all_scales=True,
+                 )
+        self.conv1 = nn.Conv2d(128, 1, kernel_size=3, stride=1, padding=1)
         print(self.cfg.configu) 
 
     def forward(self, x: BatchedViews):
-        return self.conv1(x['images'][:,0]).unsqueeze(1)
+        print(x['images'].shape)
+        x = self.backbone(x['images'][:,0])
+        print('\n\n\n')
+        print(len(x))
+        for i in range(len(x)):
+            print(x[i].shape)
+        exit()
+        return self.conv1(x[0]).unsqueeze(1)
 
